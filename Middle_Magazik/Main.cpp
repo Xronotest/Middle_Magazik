1ec0dc5a59af0a7c83ed5b2cf93c3e5a4dbbd894
Imanbay <yakaleks96@gmail.com>
2025-11-11 09:07:45 +0500

    Добавьте файлы проекта.

---
+++ Middle_Magazik/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <iomanip>
+
+void Start();
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+
+	Start();
+
+
+
+	return 0;
+}
+
+
+void Start() 
+{
+
+}
